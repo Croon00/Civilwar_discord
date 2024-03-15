88050abd8688eff199aa72d6297b8826ce01d094
현승민 <smhyun128@naver.com>
2024-03-15 20:06:12 +0900

    Feat: 명령어 확인 추가

--- main.py
+++ main.py
@@ -335,7 +335,21 @@ async def check_user_score(ctx, username: str):
     else: await ctx.send(f'{username} 사용자 정보는 없습니다.')
 
 
-
+@bot.command(name="명령어")
+async def my_help(ctx):
+    embed = discord.Embed(title="Custom Help", description="명령어들:")
+    embed.add_field(name="사용자추가", value="(이름, 점수)을 통해 사용자 정보 추가")
+    embed.add_field(name="모든사용자", value="모든 사용자 점수 정보 확인")
+    embed.add_field(name="전적", value="(이름)을 통해 최근 5경기 전적 확인")
+    embed.add_field(name="점수", value="(이름)을 통해 현재 점수 확인 ")
+    embed.add_field(name="점수수정", value="(이름, 점수)를 통해 해당 사람 점수 수정")
+    embed.add_field(name="최근매치", value="가장 최근 시간으로 5 경기 매치 결과 확인")
+    embed.add_field(name="팀나누기", value="(이름 * 10)을 통해 5대 5로 최소한의 점수로 팀 나누기")
+    embed.add_field(name="팀1승리", value="가장 최근 나눈 팀을 기준으로 1팀 승리 ")
+    embed.add_field(name="팀2승리", value="가장 최근 나눈 팀을 기준으로 2팀 승리 ")
+    # Add more fields as needed
+    
+    await ctx.send(embed=embed)
 
 # 봇 종료 시 SQLite 연결 해제
 @bot.event
